c6c26fff51bad1328a50517975774deb265b520c
Michelle Baeten <michellebaeten@hotmail.com>
2020-09-15 10:28:21 +0100

    Updated YAML

---
+++ createscratchorg.sh
@@ -0,0 +1,21 @@
+#First we create a scratch org
+if [ $# -eq 0 ]; then
+sfdx force:org:create -f config/project-scratch-def.json -s
+else
+sfdx force:org:create -f config/project-scratch-def.json -s -a $1
+fi
+#Install required packages
+#Financial Services cloud
+#sfdx force:package:install --package 04t1E000000cmtN --noprompt --wait=30
+#push source code
+sfdx force:source:push
+#assign permission set to the default user
+sfdx force:user:permset:assign -n ebikes
+#import sample data
+sfdx force:data:tree:import -p ./data/sample-data-plan.json
+#deploy metadata that isn't available in source format (yet)
+sfdx force:mdapi:deploy -d mdapiDeploy/unpackaged -w 5
+#publish community
+sfdx force:community:publish -n E-Bikes
+#open community
+sfdx force:org:open
